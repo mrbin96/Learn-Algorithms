5ddb0e3b32ee2e5932a781c76493e5c01e373a4f
765262909 <765262909@qq.com>
2022-03-03 17:44:25 +0800

    关于冒泡排序的Python实现

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -5,7 +5,7 @@
 
 def BubbleSort(arr):
     for i in range(1,len(arr)):
-        for j in range(i,len(arr)-i):
+        for j in range(0,len(arr)-i):
             if arr[j] > arr[j+1]:
                 arr[j],arr[j+1] = arr[j+1],arr[j]
     return arr
